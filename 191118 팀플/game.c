475964cf5943aea2789fde9b79421eb667e87e3d
maminwu <minwoo9594@gmail.com>
2019-11-25 18:21:36 +0900

    화살표 수정

--- 191118 팀플/game.c
+++ 191118 팀플/game.c
@@ -7,16 +7,16 @@ int keyControl()
 	char temp = getch(); //getch()함수는 엔터안눌러도 키가 눌리면 바로 반환
 	//키가 입력되면 바로 temp변수에 임시로 값을 저장하고 if문에서 확인 해당문자 맞으면 값반환
 
-	if (temp == 'w' || temp == 'W') {
+	if (temp == 'w' || temp == 'W' || temp == 72) {
 		return UP;
 	}
-	else if (temp == 's' || temp == 'S') {
+	else if (temp == 's' || temp == 'S' || temp == 80) {
 		return DOWN;
 	}
-	else if (temp == 'a' || temp == 'A') {
+	else if (temp == 'a' || temp == 'A' || temp == 75) {
 		return LEFT;
 	}
-	else if (temp == 'd' || temp == 'D') {
+	else if (temp == 'd' || temp == 'D' || temp == 77) {
 		return RIGHT;
 	}
 	else if (temp == 13) {
